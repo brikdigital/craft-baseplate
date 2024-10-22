0045ec580c3728b9284867bcd2ae8d0174de09a6
Alyxia Sother <alyxia@riseup.net>
2024-10-22 16:36:18 +0200

    Add the Twig plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
   ...require('eslint-config-dmitmel/prettier.config.js'),
+  plugins: ['prettier-plugin-tailwindcss', '@destination/prettier-plugin-twig'],
+  indentSchema: true,
+  overrides: [
+    {
+      files: '*.twig',
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 };
