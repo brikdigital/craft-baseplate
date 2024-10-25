0515ca18cd061e771113eea35cf066db829b1746
Alyxia Sother <alyxia@riseup.net>
2024-10-25 14:09:04 +0200

    Remove the Tailwind & Twig plugins from Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   ...require('eslint-config-dmitmel/prettier.config.js'),
-  plugins: ['prettier-plugin-tailwindcss', '@destination/prettier-plugin-twig'],
-  indentSchema: true,
   overrides: [
     {
       files: '*.twig',
