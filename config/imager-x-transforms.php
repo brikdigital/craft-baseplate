85cd7b4cf0e553dd9fdb761c531f9bd33b84d59a
Joost Clevis <joost@brik.digital>
2024-10-29 12:30:06 +0100

    Update image atom

--- config/imager-x-transforms.php
+++ config/imager-x-transforms.php
@@ -9,110 +9,151 @@ function getImagePosition($image) {
 
 return [
     'default' => [
-        'transforms' => [
-            ['width' => 3800],
-            ['width' => 1600],
-        ],
+        'displayName' => 'Default image',
+        'transforms' => [400, 3800],
         'defaults' => [
-            'jpegQuality' => 70,
             'position' => static function ($image) {
                 return getImagePosition($image);
             }
         ]
     ],
-    'icon' => [
-        'transforms' => [
-            ['width' => 200],
-            ['width' => 100],
-        ],
+    'heroImage' => [
+        'displayName' => 'Hero image',
+        'transforms' => [1600, 3800, 16/9],
         'defaults' => [
-            'fillInterval' => '200',
             'position' => static function ($image) {
                 return getImagePosition($image);
             }
         ]
     ],
-    'hero' => [
-        'transforms' => [
-            ['width' => 3800],
-            ['width' => 1600],
-        ],
+    'heroImageWebp' => [
+        'displayName' => 'Hero image',
+        'transforms' => 'heroImage',
         'defaults' => [
-            'ratio' => 9/4,
-            'jpegQuality' => 50,
+            'format' => 'webp'
+        ]
+    ],
+    'heroImageMobile' => [
+        'displayName' => 'Hero image',
+        'transforms' => [400, 1400, 1],
+        'defaults' => [
             'position' => static function ($image) {
                 return getImagePosition($image);
             }
         ]
     ],
-    'heroMobile' => [
-        'transforms' => [
-            ['width' => 1400],
-            ['width' => 400],
-        ],
+    'heroImageMobileWebp' => [
+        'displayName' => 'Hero image',
+        'transforms' => 'heroImageMobile',
         'defaults' => [
-            'ratio' => 78/115,
-            'jpegQuality' => 50,
-            'position' => static function ($image) {
-                return getImagePosition($image);
-            }
-        ]
-    ],
-    'textImage' => [
-        'transforms' => [
-            ['width' => 3800],
-            ['width' => 1600],
+            'format' => 'webp'
         ],
-        'defaults' => [
-            'jpegQuality' => 70,
-            'ratio' => 29/31,
-            'position' => static function ($image) {
-                return getImagePosition($image);
-            }
-        ]
     ],
-    'aspect-1/1' => [
-        'transforms' => [
-            ['ratio' => 1/1, 'height' => 1200],
-        ],
-        'defaults' => [
-            'jpegQuality' => 70,
-            'position' => static function ($image) {
-                return getImagePosition($image);
-            }
-        ]
-    ],
-    'aspect-4/3' => [
-        'transforms' => [
-            ['ratio' => 4/3, 'height' => 1200],
-        ],
-        'defaults' => [
-            'jpegQuality' => 70,
-            'position' => static function ($image) {
-                return getImagePosition($image);
-            }
-        ]
-    ],
-    'aspect-16/9' => [
-        'transforms' => [
-            ['ratio' => 16/9, 'height' => 1200],
-        ],
-        'defaults' => [
-            'jpegQuality' => 70,
-            'position' => static function ($image) {
-                return getImagePosition($image);
-            }
-        ]
-    ],
-    'aspect-5/2' => [
-        'transforms' => [
-            ['ratio' => 5/2, 'height' => 1200],
-        ],
-        'defaults' => [
-            'jpegQuality' => 70,
-            'position' => static function ($image) {
-                return getImagePosition($image);
-            }
-        ]
-    ],
+    // 'default' => [
+    //     'transforms' => [
+    //         ['width' => 3800],
+    //         ['width' => 1600],
+    //     ],
+    //     'defaults' => [
+    //         'jpegQuality' => 70,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'icon' => [
+    //     'transforms' => [
+    //         ['width' => 200],
+    //         ['width' => 100],
+    //     ],
+    //     'defaults' => [
+    //         'fillInterval' => '200',
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'hero' => [
+    //     'transforms' => [
+    //         ['width' => 3800],
+    //         ['width' => 1600],
+    //     ],
+    //     'defaults' => [
+    //         'ratio' => 9/4,
+    //         'jpegQuality' => 50,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'heroMobile' => [
+    //     'transforms' => [
+    //         ['width' => 1400],
+    //         ['width' => 400],
+    //     ],
+    //     'defaults' => [
+    //         'ratio' => 78/115,
+    //         'jpegQuality' => 50,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'textImage' => [
+    //     'transforms' => [
+    //         ['width' => 3800],
+    //         ['width' => 1600],
+    //     ],
+    //     'defaults' => [
+    //         'jpegQuality' => 70,
+    //         'ratio' => 29/31,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'aspect-1/1' => [
+    //     'transforms' => [
+    //         ['ratio' => 1/1, 'height' => 1200],
+    //     ],
+    //     'defaults' => [
+    //         'jpegQuality' => 70,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'aspect-4/3' => [
+    //     'transforms' => [
+    //         ['ratio' => 4/3, 'height' => 1200],
+    //     ],
+    //     'defaults' => [
+    //         'jpegQuality' => 70,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'aspect-16/9' => [
+    //     'transforms' => [
+    //         ['ratio' => 16/9, 'height' => 1200],
+    //     ],
+    //     'defaults' => [
+    //         'jpegQuality' => 70,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
+    // 'aspect-5/2' => [
+    //     'transforms' => [
+    //         ['ratio' => 5/2, 'height' => 1200],
+    //     ],
+    //     'defaults' => [
+    //         'jpegQuality' => 70,
+    //         'position' => static function ($image) {
+    //             return getImagePosition($image);
+    //         }
+    //     ]
+    // ],
 ];
