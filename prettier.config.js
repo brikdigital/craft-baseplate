9feda3c26389476a68ea6b7066eb9b4496be9416
Joost Clevis <joost@brik.digital>
2024-11-06 14:10:36 +0100

    Update prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   ...require('eslint-config-dmitmel/prettier.config.js'),
-  overrides: [
-    {
-      files: '*.twig',
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
 };
