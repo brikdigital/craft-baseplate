d2c212d57b1528eb0fab9b561b7be1a8588a3fda
Alyxia Sother <alyxia@riseup.net>
2025-07-01 11:18:48 +0200

    *Actually* set up Tailwind sorting

--- prettier.config.js
+++ prettier.config.js
@@ -3,9 +3,18 @@ module.exports = {
 
 	useTabs: true,
 
-	plugins: ['prettier-plugin-tailwindcss'],
+	plugins: ['prettier-plugin-tailwindcss', '@ttskch/prettier-plugin-tailwindcss-anywhere'],
 
 	// prettier-plugin-tailwindcss
 	tailwindStylesheet: './src/pcss/app.pcss',
 	tailwindConfig: './tailwind.config.ts',
+
+	overrides: [
+		{
+			files: '*.twig',
+			options: {
+				parser: 'anywhere',
+			},
+		},
+	],
 };
