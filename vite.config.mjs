cd695cf16b5c6738a3119e1926697e0f43f641cb
Alyxia Sother <alyxia@riseup.net>
2025-07-16 12:05:56 +0200

    Re-add the Storybook Marker plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,68 +13,68 @@ import tailwindcss from '@tailwindcss/postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '' : '/dist/',
-  publicDir: path.resolve(__dirname, './src/public'),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@css': path.resolve(__dirname, 'src/pcss'),
-      '@ts': path.resolve(__dirname, 'src/ts'),
-    },
-  },
+	base: command === 'serve' ? '' : '/dist/',
+	publicDir: path.resolve(__dirname, './src/public'),
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@css': path.resolve(__dirname, 'src/pcss'),
+			'@ts': path.resolve(__dirname, 'src/ts'),
+		},
+	},
 
-  build: {
-    target: 'es2023',
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    manifest: true,
-    sourcemap: false,
-    outDir: path.resolve(__dirname, 'public_html/dist/'),
-    rollupOptions: {
-      input: {
-        app: path.resolve(__dirname, 'src/ts/app.ts'),
-      },
-    },
-  },
+	build: {
+		target: 'es2023',
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+		manifest: true,
+		sourcemap: false,
+		outDir: path.resolve(__dirname, 'public_html/dist/'),
+		rollupOptions: {
+			input: {
+				app: path.resolve(__dirname, 'src/ts/app.ts'),
+			},
+		},
+	},
 
-  css: {
-    postcss: {
-      plugins: [inlineSVG, pxtorem, tailwindcss],
-    },
-  },
+	css: {
+		postcss: {
+			plugins: [inlineSVG, pxtorem, tailwindcss],
+		},
+	},
 
-  plugins: [
-    Inspect(),
-    // We don't need Critical CSS locally
-    ...(process.env.BUDDY === true || process.env.BUDDY === 'true'
-      ? [
-          critical({
-            criticalUrl: 'https://craft-baseplate.ddev.site',
-            criticalBase: './public_html/dist/critical/',
-            criticalPages: [{ uri: '/', template: 'index' }],
-            criticalConfig: {},
-          }),
-        ]
-      : []),
-    favicons({
-      logo: './src/img/favicon/favicon-src.svg',
-      inject: false,
-      outputPath: 'favicons',
-    }),
-    compression({
-      include: /\.(js|mjs|json|css|map)/i,
-      algorithms: ['gzip', 'brotliCompress', defineAlgorithm('deflate', { level: 9 })],
-    }),
-    restart({
-      reload: ['templates/**/*'],
-    }),
-  ],
+	plugins: [
+		Inspect(),
+		// We don't need Critical CSS locally
+		...(process.env.BUDDY === true || process.env.BUDDY === 'true'
+			? [
+					critical({
+						criticalUrl: 'https://craft-baseplate.ddev.site',
+						criticalBase: './public_html/dist/critical/',
+						criticalPages: [{ uri: '/', template: 'index' }],
+						criticalConfig: {},
+					}),
+				]
+			: []),
+		favicons({
+			logo: './src/img/favicon/favicon-src.svg',
+			inject: false,
+			outputPath: 'favicons',
+		}),
+		compression({
+			include: /\.(js|mjs|json|css|map)/i,
+			algorithms: ['gzip', 'brotliCompress'],
+		}),
+		restart({
+			reload: ['templates/**/*'],
+		}),
+	],
 
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    allowedHosts: ['craft-baseplate.ddev.site'],
-  },
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		strictPort: true,
+		allowedHosts: ['craft-baseplate.ddev.site'],
+	},
 }));
