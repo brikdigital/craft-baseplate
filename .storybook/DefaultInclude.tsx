2869d6e1c4ebc86f09373d16ea47924120d2fb8a
Alyxia Sother <alyxia@riseup.net>
2025-08-07 09:25:50 +0200

    Very badly support {}/[] in DefaultInclude

--- .storybook/DefaultInclude.tsx
+++ .storybook/DefaultInclude.tsx
@@ -18,10 +18,22 @@ export default function DefaultInclude() {
         {% include "${path}" with {\n${Object.keys(story.argTypes)
 					.map((key) => {
 						let value = story.argTypes[key].table?.defaultValue?.summary;
-						if (!value) value = 'No default value was set. Configure table.defaultValue.summary!';
+						if (!value && value !== '')
+							value = 'No default value was set. Configure table.defaultValue.summary!';
+
+						if (value.startsWith('[') || value.startsWith('{')) {
+							console.log(value);
+							try {
+								value = JSON.parse(value);
+							} catch (e) {
+								value = 'Whatever default value was provided here failed JSON.parse.';
+							}
+						}
+
 						value = match(typeof value)
 							.with('string', () => `"${value}"`)
 							.with('number', 'boolean', () => value)
+							.with('object', () => JSON.stringify(value))
 							.otherwise(
 								() => `"The type of this option's default value wasn't recognized. Report this!"`,
 							);
